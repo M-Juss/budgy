c91016f960ed3c33093996f0181606d361a393de
M-Juss <jusssayson@gmail.com>
2025-07-21 23:06:45 +0800

    Fix Bugs

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -4,9 +4,10 @@ import { IoIosAddCircle } from "react-icons/io";
 import { RiChatHistoryFill } from "react-icons/ri";
 import { IoMdSettings } from "react-icons/io";
 import { IoLogOutSharp } from "react-icons/io5";
-import { Link } from 'react-router-dom';
+ import { useLocation, Link } from 'react-router-dom';
 
 const Sidebar = () => {
+    const location = useLocation()
 
     const [clickSidePage, setClickSidePage] = useState('')
 
@@ -14,30 +15,31 @@ const Sidebar = () => {
         {label:'Dashboard', icon: <MdDashboard className=' my-auto ' size={30}/>, link:'/'},
         {label:'Add Transaction', icon: <IoIosAddCircle className=' my-auto ' size={30}/>, link:'/transaction'},
         {label:'History', icon: <RiChatHistoryFill className=' my-auto ' size={30}/>, link:'/history'},
-        {label: 'Settings', icon:<IoMdSettings className=' my-auto' size={30}/>, link: '/setting' },
+        {label: 'Settings', icon:<IoMdSettings className=' my-auto' size={30}/>, link: '/settings' },
     ]
 
   return (
             <sidebar className='flex flex-col h-full w-70 border-r-1 '> 
             <h1 className='text-center font-medium text-3xl py-3.5'>Budgy</h1>
             {sideBar.map((item, index) => {
+                const isActive = location.pathname == item.link
                 return(
-                <Link to={item.link}>
-                    <div key={index}
+                    <Link to={item.link} key={index}>
+                    <div
                     className={`flex w-full px-4 py-3 gap-x-3  hover:bg-yellow-900 my-1 ${
-                        clickSidePage === index
+                        isActive
                         ? 'duration-200 hover:bg-yellow-950 bg-yellow-950'
                         : ''
                         }`}
-                    onClick={() => (setClickSidePage(index))} >
-                    {clickSidePage === index && (
+                     >
+                    {isActive && (
                     <>
                         <div className=' absolute left-0 h-8 w-1 bg-white'></div>
                     </>)}
                     <span>{item.icon}</span>
                     <span className='text-md my-auto text-accent-content'>{item.label}</span>
                     </div>
-                </Link>
+                    </Link>
                 )
             })}
 
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <div className='flex h-screen w-screen overflow-hidden'>
         <Sidebar/>
-        <div className='flex justify-center items-center p-10 h-full w-full'>
+        <div className='flex justify-center items-center p-10 h-full w-full '>
             <div className='h-full w-full'>
               <h1>Dashboard</h1>
             </div>
