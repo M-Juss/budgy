18c09d9f8af01f521f6cd93a2c708cac1dfb6477
M-Juss <jusssayson@gmail.com>
2025-08-05 10:07:33 +0800

    add endpoint to get all transaction which type is income

--- server/routes/userData.js
+++ server/routes/userData.js
@@ -26,7 +26,7 @@ router.get("/transaction", async (req,res) => { // get All of the Transaction
   }
 })
 
-router.get("/expenses", async (req, res) => {
+router.get("/transaction/expense", async (req, res) => {
   try {
     const result = await collection.aggregate([
       {
@@ -54,5 +54,31 @@ router.get("/expenses", async (req, res) => {
   }
 });
 
+router.get("/transaction/income", async (req, res) => {
+  try{
+    const result = await collection.aggregate([
+      {
+        $match: { type: "Income"}
+      },
+      {
+        $group: {
+          _id: "$category",
+          totalAmount: { $sum: { $toDouble: "$amount" } }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          category: "$_id",
+          amount: "$totalAmount"
+        }
+      }
+    ]).toArray()
+    res.status(200).json(result)
+  } catch (error) {
+      console.log("Error message:", error);
+      res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 export default router;
